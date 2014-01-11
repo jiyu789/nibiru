e1306bba8ecead52c688e0c4929602dd7084e1ee
wangbo <advance345@126.com>
2014-01-11 15:43:54 +0800

    增加ejs

--- init.bat
+++ init.bat
@@ -1,4 +1,5 @@
 npm install --registry http://npm.cbyun.com  express
+npm install --registry http://npm.cbyun.com  ejs
 npm install --registry http://npm.cbyun.com  mongoose
 npm install --registry http://npm.cbyun.com  express-mongoose
 npm install --registry http://npm.cbyun.com  express-resource
