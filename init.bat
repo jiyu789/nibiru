fec35f8bd9a1843cc839fb91f69d3ccd8f99d0cc
wangbo <advance345@126.com>
2014-01-11 15:24:41 +0800

    搭建项目骨架

---
+++ init.bat
@@ -0,0 +1,4 @@
+npm install --registry http://npm.cbyun.com  express
+npm install --registry http://npm.cbyun.com  mongoose
+npm install --registry http://npm.cbyun.com  express-mongoose
+npm install --registry http://npm.cbyun.com  express-resource
